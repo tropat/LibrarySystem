30652332226fa62b5b854be0361af414c0017e95
patrycja <patrycja@debian.com>
2022-03-05 13:20:54 +0100

    Starting a new java project with a form to logging into the system

---
+++ src/LogInForm.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+public class LogInForm implements Runnable {
+	private JFrame frame;
+	private JPanel mainPanel;
+	private JButton loginButton, passwordButton;
+	
+	public static void displayLogInForm() {
+		Thread display = new Thread(new LogInForm());
+		display.start();
+	}
+
+	@Override
+	public void run() {
+			frame = new JFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			loginButton = new JButton("Login");
+			passwordButton = new JButton("Password");
+			
+			mainPanel = new JPanel();
+			
+			mainPanel.add(loginButton);
+			mainPanel.add(passwordButton);
+			
+			frame.getContentPane().add(mainPanel);
+			
+			frame.pack();
+			frame.setVisible(true);
+			
+	}
+		
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	private LogInForm logInForm;
+	
+	public static void main(String[] args) {
+		Main mainProgram = new Main();
+		mainProgram.logInForm = new LogInForm();
+		mainProgram.logInForm.displayLogInForm();
+	}
+
+}
